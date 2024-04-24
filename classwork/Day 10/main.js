a4c3fe1a06f4f27fdec46ace5b6a7f1119cf8456
LuKee21 <gam3rlinfo@gmail.com>
2024-04-24 21:55:03 +0400

    'new'

---
+++ classwork/Day 10/main.js
@@ -0,0 +1,22 @@
+function multiplication(num1,num2,num3) {
+    console.log(num1*num2*num3);
+}
+
+multiplication(5,3,5)
+
+function fullname(name, lastname){
+    const fname = name + ' ' + lastname
+    return fname
+}
+
+console.log(fullname('Luke', 'Dzotsenidze'))
+
+
+function multiply(num1, num2) {
+    const multiplied = num1*num2
+    return multiplied
+}
+
+const multiplied2 = multiply(5, 10)
+
+console.log(multiplied2 * 2)
